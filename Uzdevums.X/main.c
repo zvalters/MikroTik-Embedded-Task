c18e9fb9de70e6e7e3022bd38ec5b30e7d9d687b
Valters Ivars <walter@Ingas-MacBook-Air.local>
2020-05-08 14:38:59 +0100

    Initial commit

---
+++ Uzdevums.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: walter
+ *
+ * Created on May 8, 2020, 2:36 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
